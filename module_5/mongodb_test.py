ae0e0120e00cdf32ed064c31c0eaf258ad1ce405
Lanedra <lanmoore@my365.bellevue.edu>
2023-06-29 20:58:53 -0500

    Added comments

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -4,15 +4,21 @@
    # Test program for connecting to Atlas
    # mongodb_test.py
 
+#import
 from pymongo import MongoClient
 
+#connection string
 url = "mongodb+srv://admin:admin@cluster0.ok0kjbr.mongodb.net/?retryWrites=true&w=majority"
 
+#connect to cluster
 client = MongoClient(url)
 
+#connect database
 db = client.pytech
 
+#show collections
 print("\n -- Pytech Collection List --")
 print(db.list_collection_names())
 
+#exit message
 input("\n\n   End of program, press any key to exit...")
